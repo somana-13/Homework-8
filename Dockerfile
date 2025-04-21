020a8af7e2c9a58553bfec07bc621a5a82b367a0
somana-13 <somanaveeramaneni@gmail.com>
2025-04-21 18:05:26 -0400

    Initial CI/CD setup: app, Dockerfile & workflow

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+# Dockerfile
+FROM python:3.9-slim
+WORKDIR /app
+COPY main.py .
+CMD ["python", "main.py"]
---
+++ main.py
@@ -0,0 +1,6 @@
+# main.py
+def hello():
+    return "Hello from Homework-8!"
+
+if __name__ == "__main__":
+    print(hello())
